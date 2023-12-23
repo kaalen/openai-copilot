468d2f8a2e4356f38efc530c03035f13dc5c633c
Alenka Caserman <alenka.caserman@avanade.com>
2023-12-23 07:23:34 +0000

    Add "Any time" option to filter by updated in last x timeperiod, support for anything older than 30 days

--- app/frontend/src/components/FileStatus/FileStatus.tsx
+++ app/frontend/src/components/FileStatus/FileStatus.tsx
@@ -23,6 +23,7 @@ const dropdownTimespanOptions = [
     { key: '24hours', text: '24 hours' },
     { key: '7days', text: '7 days' },
     { key: '30days', text: '30 days' },
+    { key: 'anytime', text: 'Any time' },
   ];
 
 const dropdownFileStateOptions = [
