abf711dc3b3771d8925eab912e6ab100beb8e6b4
kalinky <alenka.caserman@avanade.com>
2024-01-23 13:50:45 +0000

    Added theme provider to get some basic styling for dialog component

--- app/frontend/src/index.tsx
+++ app/frontend/src/index.tsx
@@ -5,6 +5,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { HashRouter, Routes, Route } from "react-router-dom";
 import { initializeIcons } from "@fluentui/react";
+import { ThemeProvider } from "@fluentui/react";
 
 import "./index.css";
 
